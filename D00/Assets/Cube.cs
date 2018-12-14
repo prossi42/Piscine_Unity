35ca78e4b43983957b40865249cc56de78b965da
Pierre ROSSI <prossi@e1r11p11.42.fr>
2018-12-14 17:24:33 +0100

    ex00 ex01 done

--- D00/Assets/Cube.cs
+++ D00/Assets/Cube.cs
@@ -7,9 +7,9 @@ public class Cube : MonoBehaviour {
     public static int isInstanciedA;
     public static int isInstanciedS;
     public static int isInstanciedD;
-    private float timeA;
-    private float timeS;
-    private float timeD;
+    private Vector3 down = new Vector3(0.0f, -0.01f, 0.0f);
+    private float precision;
+    private float result;
 
 	// Use this for initialization
 	void Start () {
@@ -18,17 +18,110 @@ public class Cube : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (gameObject.name == CubeSpawner.aInstance.name)
-        {
-            print("ta mere");
-        }
-        else if (gameObject.name == CubeSpawner.sInstance.name)
-        {
-            print("la");
-        }
-        else
-        {
-            print("pute");
-        }
+        UpdateA();
+        UpdateS();
+        UpdateD();
 	}
+
+    void UpdateA(){
+
+        if (CubeSpawner.aInstance && gameObject.name == CubeSpawner.aInstance.name)
+        {
+            if (isInstanciedA == 1)
+            {
+                down.y = CubeSpawner.downSpeedA;
+                CubeSpawner.aInstance.transform.Translate(down);
+                if (CubeSpawner.aInstance.transform.position.y < 4.6f)
+                {
+                    Destroy(CubeSpawner.aInstance);
+                    isInstanciedA = -1;
+                }
+            }
+            if (Input.GetKeyDown("a"))
+            {
+                precision = CubeSpawner.aInstance.transform.position.y - 5f;
+                if (precision < 0)
+                    precision *= -1;
+                precision = precision / 0.5f;
+                if (precision > 1)
+                    result = 0;
+                else
+                {
+                    precision *= 100;
+                    result = 100 - precision;
+                    Destroy(CubeSpawner.aInstance);
+                    isInstanciedA = -1;
+                }
+                Debug.Log("Precision: " + result);
+            }
+        }
+    }
+
+    void UpdateS()
+    {
+        if (CubeSpawner.sInstance && gameObject.name == CubeSpawner.sInstance.name)
+        {
+            if (isInstanciedS == 1)
+            {
+                down.y = CubeSpawner.downSpeedS;
+                CubeSpawner.sInstance.transform.Translate(down);
+                if (CubeSpawner.sInstance.transform.position.y < 4.6f)
+                {
+                    Destroy(CubeSpawner.sInstance);
+                    isInstanciedS = -1;
+                }
+            }
+            if (Input.GetKeyDown("s"))
+            {
+                precision = CubeSpawner.sInstance.transform.position.y - 5f;
+                if (precision < 0)
+                    precision *= -1;
+                precision = precision / 0.5f;
+                if (precision > 1)
+                    result = 0;
+                else
+                {
+                    precision *= 100;
+                    result = 100 - precision;
+                    Destroy(CubeSpawner.sInstance);
+                    isInstanciedS = -1;
+                }
+                Debug.Log("Precision: " + result);
+            }
+        }
+    }
+
+    void UpdateD()
+    {
+        if (CubeSpawner.dInstance && gameObject.name == CubeSpawner.dInstance.name)
+        {
+            if (isInstanciedD == 1)
+            {
+                down.y = CubeSpawner.downSpeedD;
+                CubeSpawner.dInstance.transform.Translate(down);
+                if (CubeSpawner.dInstance.transform.position.y < 4.6f)
+                {
+                    Destroy(CubeSpawner.dInstance);
+                    isInstanciedD = -1;
+                }
+            }
+            if (Input.GetKeyDown("d"))
+            {
+                precision = CubeSpawner.dInstance.transform.position.y - 5f;
+                if (precision < 0)
+                    precision *= -1;
+                precision = precision / 0.5f;
+                if (precision > 1)
+                    result = 0;
+                else
+                {
+                    precision *= 100;
+                    result = 100 - precision;
+                    Destroy(CubeSpawner.dInstance);
+                    isInstanciedD = -1;
+                }
+                Debug.Log("Precision: " + result);
+            }
+        }
+    }
 }
--- D00/Assets/CubeSpawner.cs
+++ D00/Assets/CubeSpawner.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class CubeSpawner : MonoBehaviour {
 
-    private Vector3 startPosition = new Vector3();
-    private Vector3 downtranslation = new Vector3(0f, 0.01f, 0f);
-    public GameObject aPrefab;
-    public GameObject sPrefab;
-    public GameObject dPrefab;
-    public static GameObject aInstance;
-    public static GameObject sInstance;
-    public static GameObject dInstance;
+    private         Vector3     startPosition = new Vector3();
+    public          GameObject  aPrefab;
+    public          GameObject  sPrefab;
+    public          GameObject  dPrefab;
+    public  static  GameObject  aInstance;
+    public  static  GameObject  sInstance;
+    public  static  GameObject  dInstance;
+    private float repopTime;
+    private float countTime;
+    public static float downSpeedA;   
+    public static float downSpeedS;   
+    public static float downSpeedD;   
 
 	// Use this for initialization
 	void Start () {
@@ -30,16 +34,61 @@ public class CubeSpawner : MonoBehaviour {
         {
             if (Cube.isInstanciedA == 0)
                 UpdateA();
+            else if (Cube.isInstanciedA == -1)
+            {
+                repopTime = Random.Range(0.01f, 3.0f);
+                countTime = 0f;
+                Cube.isInstanciedA = -2;
+            }
+            else if (Cube.isInstanciedA == -2)
+            {
+                countTime += Time.deltaTime;
+                if (countTime >= repopTime)
+                {
+                    Cube.isInstanciedA = 0;
+                    countTime = 0f;
+                }
+            }
         }
-        else if (gameObject.name == "spawnB")
+        else if (gameObject.name == "spawnS")
         {
             if (Cube.isInstanciedS == 0)
                 UpdateS();
+            else if (Cube.isInstanciedS == -1)
+            {
+                repopTime = Random.Range(0.01f, 3.0f);
+                countTime = 0f;
+                Cube.isInstanciedS = -2;
+            }
+            else if (Cube.isInstanciedS == -2)
+            {
+                countTime += Time.deltaTime;
+                if (countTime >= repopTime)
+                {
+                    Cube.isInstanciedS = 0;
+                    countTime = 0f;
+                }
+            }
         }
         else
         {
             if (Cube.isInstanciedD == 0)
                 UpdateD();
+            else if (Cube.isInstanciedD == -1)
+            {
+                repopTime = Random.Range(0.01f, 3.0f);
+                countTime = 0f;
+                Cube.isInstanciedD = -2;
+            }
+            else if (Cube.isInstanciedD == -2)
+            {
+                countTime += Time.deltaTime;
+                if (countTime >= repopTime)
+                {
+                    Cube.isInstanciedD = 0;
+                    countTime = 0f;
+                }
+            }
         }
     }
 
@@ -47,6 +96,7 @@ public class CubeSpawner : MonoBehaviour {
 
         aInstance = Instantiate(aPrefab);
         aInstance.transform.position = startPosition;
+        downSpeedA = Random.Range(-0.03f, -0.005f);
         Cube.isInstanciedA = 1;
     }
 
@@ -54,6 +104,7 @@ public class CubeSpawner : MonoBehaviour {
 
         sInstance = Instantiate(sPrefab);
         sInstance.transform.position = startPosition;
+        downSpeedS = Random.Range(-0.03f, -0.005f);
         Cube.isInstanciedS = 1;
     }
 
@@ -61,6 +112,7 @@ public class CubeSpawner : MonoBehaviour {
 
         dInstance = Instantiate(dPrefab);
         dInstance.transform.position = startPosition;
+        downSpeedD = Random.Range(-0.03f, -0.005f);
         Cube.isInstanciedD = 1;
     }
 }
